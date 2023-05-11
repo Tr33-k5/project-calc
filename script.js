b7a7c93ba933a1273507fdbb631a283d48f61b7f
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-05-12 01:51:17 +0200

    feat: store first operand when an operator is pressed by user

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 const displayExpression = document.getElementById('expression');
 const displayResult = document.getElementById('result');
-const displayablebuttons = document.querySelectorAll('#displayable');
+const displayableButtons = document.querySelectorAll('.displayable');
+const operatorButtons = document.getElementsByName('operators');
 const allClearButton = document.getElementById('clear');
 const clearButton = document.getElementById('sign');
 const equalButton = document.getElementById('equal');
+let operand1,operand2,operator;
 
 const sum = (x,y) => (x + y);
 const subtract = (x,y) => (x - y);
 const multiply = (x,y) => (x * y);
 const divide = (x,y) => (x / y);
-const operate = (operator,x,y) => {
+const operate = (operator,operand1,operand2) => {
    switch (operator) {
       case '+':
-        sum(x,y);
+        sum(operand1,operand2);
         break;
       case '-':
-         subtract(x,y);
+         subtract(operand1,operand2);
         break;
       case '*':
-         multiply(x,y);
+         multiply(operand1,operand2);
         break;
       case '*':
-         sum(x,y);
+         sum(operand1,operand2);
       break;
       case '/':
-         divide(x,y);
+         divide(operand1,operand2);
          break; 
       default:
         console.console.log(('ERROR'));
@@ -36,12 +38,23 @@ const allClear = () => {
    displayExpression.textContent = '';
    displayResult.textContent = '';
 }
-const clear = () => {
-   displayExpression.textContent = (displayExpression.textContent).slice(0, -2);
+const clear = () => displayExpression.textContent = (displayExpression.textContent).slice(0, -2);
+
+function getTerms(){
+   let expression = displayExpression.textContent.split('');
+   expression = expression.filter(entry => entry.trim() != '');
+   operator = expression.pop();
+   operand1 = expression.join('');
+   console.log('operand: '+operand1);
+   console.log('operator: '+operator);
 }
 
-displayablebuttons.forEach(button => button.addEventListener('click',() => display(button.textContent)));
+displayableButtons.forEach(button => button.addEventListener('click',() => display(button.textContent)));
+
+operatorButtons.forEach(button => button.addEventListener('click',() => getTerms()));
 
 allClearButton.addEventListener('click',() => allClear());
 
 clearButton.addEventListener('click',() => clear());
+
+equalButton.addEventListener('click',() => getTerms());

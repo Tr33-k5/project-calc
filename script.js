9107f0f3a3d618e51b464c474195adfed05a1b57
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-05-16 20:05:16 +0200

    fix: huge bug fix to pourcent feature

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ const acButton = document.getElementById('allclear');
 const cButton = document.getElementById('clear');
 const equalButton = document.getElementById('equal');
 const pointButton = document.getElementById('point');
+const pourcentButton = document.getElementById('pourcent');
 let firstOperand,secondOperand,theOperator;
 
 function countDecimals(decimal){
-   let num = parseFloat(decimal); // First convert to number to check if whole
+   // First convert to number to check if whole
+   let num = parseFloat(decimal);
    if(Number.isInteger(num) === true){ return 0 }
-   let text = num.toString(); // Convert back to string and check for "1e-8" numbers
+   // Convert back to string and check for "1e-8" numbers
+   let text = num.toString();
    if(text.indexOf('e-') > -1){
       let [base, trail] = text.split('e-');
       let deg = parseInt(trail, 10);
@@ -19,8 +22,18 @@ function countDecimals(decimal){
    }
    else{
       let index = text.indexOf(".");
-      return text.length - index - 1; // Otherwise use simple string function to count
+      // Otherwise use simple string function to count
+      return text.length - index - 1;
    }
+}
+
+const hasPourcent = (array) => {
+   const hasPourcent = ['%'].some(item => array.includes(item));
+   if(hasPourcent){ 
+      array.pop();
+      return (+(array.join('')) * 0.01).toString();
+   }
+   else return array.join('');
 }
 
 const sum = (x,y) => {
@@ -98,7 +111,7 @@ const clearOne = () => {
 
 function checkOperatorToOperate(operator){
    const expression = resultElem.textContent;
-   const checkFor = ['+','-','*','/','%'];
+   const checkFor = ['+','-','*','/'];
    const arrayExpression = expression.split('');
    // check if the expression has an operator in it
    const hasSome = checkFor.some(item => arrayExpression.includes(item));
@@ -111,7 +124,7 @@ function checkOperatorToOperate(operator){
 
       // Get the displayed first operand
       const arrayFirstOperand = arrayExpression.slice(0,index);
-      firstOperand = arrayFirstOperand.join('');
+      firstOperand = hasPourcent(arrayFirstOperand);
 
       // Check if there's a second operand
       index += 1;
@@ -122,7 +135,8 @@ function checkOperatorToOperate(operator){
 
       // Get the displayed second operand
       const arraySecondOperand = arrayExpression.slice(index);
-      secondOperand = arraySecondOperand.join('');
+      secondOperand = hasPourcent(arraySecondOperand);
+
       displayExpression(resultElem.textContent);
 
       let result = operate(theOperator,firstOperand,secondOperand);
@@ -137,70 +151,92 @@ function checkOperatorToOperate(operator){
 
 function checkExpressionToOperate(){
    const expression = resultElem.textContent;
-   const checkFor = ['+','-','*','/','%'];
+   const checkForOperator = ['+','-','*','/'];
    const arrayExpression = expression.split('');
    // check if the expression has an operator in it
-   const hasSome = checkFor.some(item => arrayExpression.includes(item));
+   const hasSome = checkForOperator.some(item => arrayExpression.includes(item));
 
    //true => operate()
    if(hasSome){
       // Get the displayed operator
-      theOperator = checkFor.find(item => arrayExpression.includes(item));
+      theOperator = checkForOperator.find(item => arrayExpression.includes(item));
       let index = arrayExpression.indexOf(theOperator);
 
       // Get the displayed first operand
       const arrayFirstOperand = arrayExpression.slice(0,index);
-      firstOperand = arrayFirstOperand.join('');
 
-      let result;
-      if(theOperator === '%'){
-         result = operate('*',firstOperand,'0.01');
-      }
-      else{
-         // Get the displayed second operand
-         index += 1;
-         const arraySecondOperand = arrayExpression.slice(index);
-         secondOperand = arraySecondOperand.join('');
-         result = operate(theOperator,firstOperand,secondOperand);
-      }
+      firstOperand = hasPourcent(arrayFirstOperand);
+
+      // Get the displayed second operand
+      index += 1;
+      const arraySecondOperand = arrayExpression.slice(index);
+
+      secondOperand = hasPourcent(arraySecondOperand);
+
+      result = operate(theOperator,firstOperand,secondOperand);
 
       if(result === 'nani?!'){return}
       
       displayExpression(resultElem.textContent);
       resultElem.textContent = result;
    }
+      //true => operate with one operand only
+      else{
+         firstOperand = hasPourcent(arrayExpression);
+         let result = operate('+',firstOperand,'0');
+         resultElem.textContent = result;
+      }
 }
 
 displayableButtons.forEach(button => button.addEventListener('click',() => {
    displayResult(button.textContent);
    if(button.id === 'point'){
+      // disable .button, =button, operator buttons
       pointButton.setAttribute("disabled", "disabled");
       equalButton.setAttribute("disabled", "disabled");
       operatorButtons.forEach(button => button.setAttribute("disabled", "disabled"));
    }
    else{
+      // enable =button, operator buttons
       equalButton.removeAttribute("disabled");
       operatorButtons.forEach(button => button.removeAttribute("disabled"));
    }
+   // enable %button
+   pourcentButton.removeAttribute("disabled");
 }));
 
 operatorButtons.forEach(button => button.addEventListener('click',() => {
    checkOperatorToOperate(button.textContent);
+   // enable .button, numpad buttons
    pointButton.removeAttribute("disabled");
+   displayableButtons.forEach(button => button.removeAttribute("disabled"));
 }));
 
 acButton.addEventListener('click',() => {
    clearAll();
+   // enable .button, disable operator buttons
    pointButton.removeAttribute("disabled");
    operatorButtons.forEach(button => button.removeAttribute("disabled"));
+   displayableButtons.forEach(button => button.removeAttribute("disabled"));
 });
 
 cButton.addEventListener('click',() => {
    clearOne();
+   // enable .button
    pointButton.removeAttribute("disabled");
+   displayableButtons.forEach(button => button.removeAttribute("disabled"));
 });
 
 equalButton.addEventListener('click',() => {
    checkExpressionToOperate();
+   // enable .button
    pointButton.removeAttribute("disabled");
+});
+
+pourcentButton.addEventListener('click',() => {
+   displayResult(pourcentButton.textContent);
+   // Disable %button, .button, numpad buttons
+   pourcentButton.setAttribute("disabled", "disabled");
+   pointButton.setAttribute("disabled", "disabled");
+   displayableButtons.forEach(button => button.setAttribute("disabled", "disabled"));
 });

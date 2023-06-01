16c182013dda8e16bf0871b5f3e4c14d2e61176a
Tr33-k5 <103614587+Tr33-k5@users.noreply.github.com>
2023-06-01 17:21:42 +0200

    refactor : code cleaning

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ const cButton = document.getElementById('clear');
 const equalButton = document.getElementById('equal');
 const pointButton = document.getElementById('point');
 const pourcentButton = document.getElementById('pourcent');
-let firstOperand,secondOperand,theOperator;
+let firstOperand, secondOperand, theOperator;
 
 const countDecimals = (decimal) => {
    // First convert to number to check if whole
    let num = parseFloat(decimal);
-   if(Number.isInteger(num) === true){ return 0 }
+   if (Number.isInteger(num) === true) { return 0 }
    // Convert back to string and check for "1e-8" numbers
    let text = num.toString();
-   if(text.indexOf('e-') > -1){
+   if (text.indexOf('e-') > -1) {
       let [base, trail] = text.split('e-');
       let deg = parseInt(trail, 10);
       return deg;
    }
-   else{
+   else {
       let index = text.indexOf(".");
       // Otherwise use simple string function to count
       return text.length - index - 1;
@@ -32,82 +32,82 @@ const displayToSecondaryView = (string) => secondaryView.textContent = string;
 const displayToMainView = (string) => mainView.textContent = string;
 
 const addToMainView = (string) => {
-   if(+string === +string && mainView.textContent != '0' || +string != +string) { 
+   if (+string === +string && mainView.textContent != '0' || +string != +string) {
       mainView.textContent += string;
    }
 }
 
 const hasPourcent = (array) => {
    const hasPourcent = ['%'].some(item => array.includes(item));
-   if(hasPourcent){ 
+   if (hasPourcent) {
       array.pop();
       return (+(array.join('')) * 0.01).toString();
    }
    else return array.join('');
 }
 
-const sum = (x,y) => {
-   let res = (+x)+(+y);
-   if(res % 1 != 0){
-      if(countDecimals(res) > 2) {
+const sum = (x, y) => {
+   let res = (+x) + (+y);
+   if (res % 1 != 0) {
+      if (countDecimals(res) > 2) {
          return res.toFixed(2);
       }
       return res.toFixed(countDecimals(res));
-   }  
+   }
    else return res;
 }
-const subtract = (x,y) => {
-   let res = (+x)-(+y);
-   if(res % 1 != 0){
-      if(countDecimals(res) > 2) {
+const subtract = (x, y) => {
+   let res = (+x) - (+y);
+   if (res % 1 != 0) {
+      if (countDecimals(res) > 2) {
          return res.toFixed(2);
       }
       return res.toFixed(countDecimals(res));
-   }  
-   else return res;
-}
-
-const multiply = (x,y) => {
-   let res = (+x)*(+y);
-   if(res % 1 != 0){
-      if(countDecimals(res) > 2) {
-         return res.toFixed(2);
-      }
-      return res.toFixed(countDecimals(res));
-   }  
+   }
    else return res;
 }
 
-const divide = (x,y) => {
-   let res = (+x)/(+y);
-   if(res % 1 != 0){
-      if(countDecimals(res) > 2) {
+const multiply = (x, y) => {
+   let res = (+x) * (+y);
+   if (res % 1 != 0) {
+      if (countDecimals(res) > 2) {
          return res.toFixed(2);
       }
       return res.toFixed(countDecimals(res));
-   }  
+   }
    else return res;
 }
 
-const operate = (operator,x,y) => {
+const divide = (x, y) => {
+   let res = (+x) / (+y);
+   if (res % 1 != 0) {
+      if (countDecimals(res) > 2) {
+         return res.toFixed(2);
+      }
+      return res.toFixed(countDecimals(res));
+   }
+   else return res;
+}
+
+const operate = (operator, x, y) => {
    switch (operator) {
       case '+':
-        return sum(x,y);
+         return sum(x, y);
       case '-':
-         return subtract(x,y);
+         return subtract(x, y);
       case '*':
-         return multiply(x,y);
+         return multiply(x, y);
       case '/':
-         if(y === '0'){
+         if (y === '0') {
             displayToSecondaryView('お前はもう死んでいる');
             displayToMainView('Omae Wa Mou Shindeiru');
             return 'nani?!'
          }
-         else return divide(x,y); 
+         else return divide(x, y);
       default:
          displayToMainView('ERROR');
-        break;
-    }  
+         break;
+   }
 }
 
 const clearAll = () => {
@@ -117,44 +117,44 @@ const clearAll = () => {
 
 const clearOne = () => {
    displayToMainView((mainView.textContent).slice(0, -1));
-   if(mainView.textContent === '') {
+   if (mainView.textContent === '') {
       displayToMainView('0');
    }
 }
 
 const checkOperatorToOperate = (operator) => {
    const expression = mainView.textContent;
-   const checkFor = ['+','-','*','/'];
+   const checkFor = ['+', '-', '*', '/'];
    const arrayExpression = expression.split('');
    // check if the expression has an operator in it
    const hasSome = checkFor.some(item => arrayExpression.includes(item));
 
    //true => operate()
-   if(hasSome) {
+   if (hasSome) {
       // Get the displayed operator
       theOperator = checkFor.find(item => arrayExpression.includes(item));
       let index = arrayExpression.indexOf(theOperator);
 
       // Get the displayed first operand
-      const arrayFirstOperand = arrayExpression.slice(0,index);
+      const arrayFirstOperand = arrayExpression.slice(0, index);
       firstOperand = hasPourcent(arrayFirstOperand);
 
       // Check if there's a second operand
       index += 1;
-      if(arrayExpression.length > index) {
+      if (arrayExpression.length > index) {
          // Get the displayed second operand
          const arraySecondOperand = arrayExpression.slice(index);
          secondOperand = hasPourcent(arraySecondOperand);
       }
       else {
-         mainView.textContent = firstOperand+operator;
+         mainView.textContent = firstOperand + operator;
          return;
       }
-      
+
       displayToSecondaryView(mainView.textContent);
 
-      let result = operate(theOperator,firstOperand,secondOperand);
-      if(+result === +result && mainView.textContent === '0') {
+      let result = operate(theOperator, firstOperand, secondOperand);
+      if (+result === +result && mainView.textContent === '0') {
          displayToMainView(result);
       }
       displayToMainView(result);
@@ -168,19 +168,19 @@ const checkOperatorToOperate = (operator) => {
 
 const checkExpressionToOperate = () => {
    const expression = mainView.textContent;
-   const checkForOperator = ['+','-','*','/'];
+   const checkForOperator = ['+', '-', '*', '/'];
    const arrayExpression = expression.split('');
    // check if the expression has an operator in it
    const hasSome = checkForOperator.some(item => arrayExpression.includes(item));
 
    //true => operate()
-   if(hasSome) {
+   if (hasSome) {
       // Get the displayed operator
       theOperator = checkForOperator.find(item => arrayExpression.includes(item));
       let index = arrayExpression.indexOf(theOperator);
 
       // Get the displayed first operand
-      const arrayFirstOperand = arrayExpression.slice(0,index);
+      const arrayFirstOperand = arrayExpression.slice(0, index);
       firstOperand = hasPourcent(arrayFirstOperand);
 
       // Get the displayed second operand
@@ -188,8 +188,8 @@ const checkExpressionToOperate = () => {
       const arraySecondOperand = arrayExpression.slice(index);
       secondOperand = hasPourcent(arraySecondOperand);
 
-      result = operate(theOperator,firstOperand,secondOperand);
-      if(result === 'nani?!') { return }
+      result = operate(theOperator, firstOperand, secondOperand);
+      if (result === 'nani?!') { return }
 
       displayToSecondaryView(mainView.textContent);
       displayToMainView(result);
@@ -197,26 +197,26 @@ const checkExpressionToOperate = () => {
    //true => operate with one operand only
    else {
       firstOperand = hasPourcent(arrayExpression);
-      let result = operate('+',firstOperand,'0');
+      let result = operate('+', firstOperand, '0');
       displayToMainView(result);
    }
 }
 
-displayableButtons.forEach(button => button.addEventListener('click',() => {
+displayableButtons.forEach(button => button.addEventListener('click', () => {
    let value = button.value;
-   if(+value === +value && mainView.textContent === '0') {
+   if (+value === +value && mainView.textContent === '0') {
       displayToMainView(value);
    }
    else {
       addToMainView(value);
    }
-   if(button.id === 'point'){
+   if (button.id === 'point') {
       // disable .button, =button, operator buttons
       pointButton.setAttribute("disabled", "disabled");
       equalButton.setAttribute("disabled", "disabled");
       operatorButtons.forEach(button => button.setAttribute("disabled", "disabled"));
    }
-   else{
+   else {
       // enable =button, operator buttons
       equalButton.removeAttribute("disabled");
       operatorButtons.forEach(button => button.removeAttribute("disabled"));
@@ -225,14 +225,14 @@ displayableButtons.forEach(button => button.addEventListener('click',() => {
    pourcentButton.removeAttribute("disabled");
 }));
 
-operatorButtons.forEach(button => button.addEventListener('click',() => {
+operatorButtons.forEach(button => button.addEventListener('click', () => {
    checkOperatorToOperate(button.textContent);
    // enable .button, numpad buttons
    pointButton.removeAttribute("disabled");
    displayableButtons.forEach(button => button.removeAttribute("disabled"));
 }));
 
-acButton.addEventListener('click',() => {
+acButton.addEventListener('click', () => {
    clearAll();
    // enable .button, disable operator buttons
    pointButton.removeAttribute("disabled");
@@ -240,20 +240,20 @@ acButton.addEventListener('click',() => {
    displayableButtons.forEach(button => button.removeAttribute("disabled"));
 });
 
-cButton.addEventListener('click',() => {
+cButton.addEventListener('click', () => {
    clearOne();
    // enable .button
    pointButton.removeAttribute("disabled");
    displayableButtons.forEach(button => button.removeAttribute("disabled"));
 });
 
-equalButton.addEventListener('click',() => {
+equalButton.addEventListener('click', () => {
    checkExpressionToOperate();
    // enable .button
    pointButton.removeAttribute("disabled");
 });
 
-pourcentButton.addEventListener('click',() => {
+pourcentButton.addEventListener('click', () => {
    addToMainView(pourcentButton.value);
    // Disable %button, .button, numpad buttons
    pourcentButton.setAttribute("disabled", "disabled");
